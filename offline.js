180a11a0e5f8f83ec09c2553a0d9c8badd5b0029
edutekno <54890481+edutekno@users.noreply.github.com>
2020-07-25 19:28:53 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1595678175,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/ratusan-sheet0.png",
+		"images/puluhan-sheet0.png",
+		"images/satuan-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/bagus1.m4a",
+		"media/bagus1.ogg",
+		"media/kalah.m4a",
+		"media/kalah.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
